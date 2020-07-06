bd0e4db3aaaf7de7411f8ae13bcc9727f399cfd7
joelxcabana <joelcabana98@gmail.com>
2020-07-06 12:15:07 -0300

    Verificacion de perfil y token para CRUD de noticias

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,37 @@
 const jwt = require('jsonwebtoken');
 const authCtrl = {}
 
+
+/*Verifica el prefil del usuario logeado, solo deja seguir si es ADMINISTRADOR*/
 authCtrl.verifyToken = async(req, res, next) => {
+
+    if (!req.headers.authorization) {
+        res.json({ message: 'Unauthorized request.' })
+    }
+    //se espera formato -> Bearer XXXX , interesa el token en pos(1) del array
+    const token = req.headers.authorization.split(' ')[1];
+    if (token === null) {
+        res.json({ message: 'no Unauthorized request.' })
+    }
+
+    const payload = jwt.verify(token, "secretkey");
+
+    console.log(payload);
+    req.userId = payload._id;
+    req.userPerfil = payload.perfil;
+
+    if (req.userPerfil == "administrador") {
+        console.log("correcto es administrador");
+        next();
+    } else {
+        res.json({
+            message: 'No autirizado - Solo Usuarios Administradores' + req.userPerfil
+        })
+    }
+}
+
+/*Verifica el prefil del usuario logeado, solo deja seguir si es ADMINISTRADOR O ADMINISTRATIVO*/
+authCtrl.verifyTokenAdmins = async(req, res, next) => {
 
         if (!req.headers.authorization) {
             res.json({ message: 'Unauthorized request.' })
@@ -19,12 +49,13 @@ authCtrl.verifyToken = async(req, res, next) => {
         req.userId = payload._id;
         req.userPerfil = payload.perfil;
 
-        if (req.userPerfil == "administrador") {
+        if (req.userPerfil == "administrador" || req.userPerfil == "administrativo") {
             console.log("correcto es administrador");
             next();
         } else {
             res.json({
-                message: 'No autirizado - Solo Usuarios Administradores' + req.userPerfil
+                message: 'No autirizado - Solo Usuarios Administradores y Administrativos' + 'su perfil es : ' +
+                    req.userPerfil
             })
         }
     }
--- routes/noticia.router.js
+++ routes/noticia.router.js
@@ -4,15 +4,16 @@ const router = express.Router();
 
 //defino controlador para el manejo de CRUD
 const noticiaCtrl = require('./../controllers/noticia.controller');
+const autCtrl = require('./../controllers/auth.controller');
 
 // definiendo rutas
 router.get('/', noticiaCtrl.getNoticias);
-router.get('/noFiltre/', noticiaCtrl.getNoticiasNotFiltre);
-router.post('/', noticiaCtrl.createNoticia);
+router.get('/noFiltre/', autCtrl.verifyTokenAdmins, noticiaCtrl.getNoticiasNotFiltre);
+router.post('/', autCtrl.verifyTokenAdmins, noticiaCtrl.createNoticia);
 router.post('/byDate/', noticiaCtrl.getNoticiasByFecha);
 router.get('/:id', noticiaCtrl.getNoticia);
-router.put('/:id', noticiaCtrl.editNoticia);
-router.delete('/:id', noticiaCtrl.deleteNoticia);
+router.put('/:id', autCtrl.verifyTokenAdmins, noticiaCtrl.editNoticia);
+router.delete('/:id', autCtrl.verifyTokenAdmins, noticiaCtrl.deleteNoticia);
 
 //filtros
 router.get('/notequals/:id', noticiaCtrl.getNoticiasIdNotEquals);
