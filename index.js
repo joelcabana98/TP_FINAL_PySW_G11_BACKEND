e7dd66565be0aaca95e4f13f64b4aebad2596508
Yesica Mendez <yesiikmm@gmail.com>
2020-07-06 11:58:53 -0300

    Definir limite de json

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var app = express();
 const { mongoose } = require('./database')
 const cors = require('cors');
 
-//middlewares
-app.use(express.json());
+// Middlewares
+app.use(express.json({limit: '50mb'}));
 app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.use('/api/usuario', require('./routes/usuario.router'));
@@ -14,10 +14,10 @@ app.use('/api/noticia', require('./routes/noticia.router'));
 app.use('/api/pago', require('./routes/pago.routes'));
 app.use('/api/servicio', require('./routes/servicio.routes'));
 
-//setting
+// Setting
 app.set('port', process.env.PORT || 3000);
 
-//starting the server
+// Starting the server
 app.listen(app.get('port'), () => {
     console.log(`Server started on port`, app.get('port'));
 });
