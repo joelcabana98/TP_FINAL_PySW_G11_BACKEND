c8699a1f8d39e0243c88e9a38bcbb33d7f2036bf
maq1 <gustavoalberto117@gmail.com>
2020-07-07 10:42:44 -0300

    Agregado comentarios a afiliado

--- controllers/afiliado.controller.js
+++ controllers/afiliado.controller.js
@@ -3,11 +3,13 @@ const afiliado = require('../models/afiliado');
 
 const afiliadoCtrl = {}
 
+//Lista todos los afiliados de la BD
 afiliadoCtrl.getAfiliados = async(req, res) => {
     afiliados = await Afiliado.find();
     res.json(afiliados);
 }
 
+//Agrega un afiliado a la BD
 afiliadoCtrl.createAfiliado = async(req, res) => {
     const afiliado = new Afiliado(req.body);
     await afiliado.save();
@@ -16,11 +18,13 @@ afiliadoCtrl.createAfiliado = async(req, res) => {
     });
 }
 
+//Retorna un afiliado por id
 afiliadoCtrl.getAfiliado = async(req, res) => {
     const afiliado = await Afiliado.findById(req.params.id);
     res.json(afiliado);
 }
 
+//Edita un afiliado
 afiliadoCtrl.editAfiliado = async(req, res) => {
     const vafiliado = new Afiliado(req.body);
     await Afiliado.findByIdAndUpdate(req.params.id, { $set:vafiliado }, { new: true});
@@ -29,6 +33,7 @@ afiliadoCtrl.editAfiliado = async(req, res) => {
     });
 }
 
+//Elimina un afiliado
 afiliadoCtrl.deleteAfiliado = async(req, res) => {
     await Afiliado.findByIdAndRemove(req.params.id);
     res.json({
@@ -36,19 +41,22 @@ afiliadoCtrl.deleteAfiliado = async(req, res) => {
     })
 }
 
+//Busca un afiliado por su EMAIL
 afiliadoCtrl.getAfiliadoByEmail = async(req, res) => {
+    //Se recibe un EMAIL como parametro
     const criteria = {
         email: req.body.email
     }
-
+    //Busca una coincidencia
     Afiliado.findOne(criteria, function(err, afil){
+        //En caso de error
         if(err){
             res.json({
                 status: 0,
                 message: 'error'
             })
         }
-
+        //En caso de que no se encuentre un afiliado
         if(!afil) {
             res.json({
                 status: 0,
@@ -56,6 +64,8 @@ afiliadoCtrl.getAfiliadoByEmail = async(req, res) => {
             })
         }
         else{
+        //En caso de que se encuentre una coincidencia
+        //Retorna un status 1, la id del afiliado y un booleano existe
             res.json({
                 status: 1,
                 message: "success",
@@ -66,19 +76,24 @@ afiliadoCtrl.getAfiliadoByEmail = async(req, res) => {
     })
 }
 
+//Busca un afiliado por su DNI
 afiliadoCtrl.getAfiliadoByDni = async(req, res) => {
+
+    //Se recibe un numero de dni como parametro
     const criteria = {
         dni: req.body.dni
     }
 
+    //Busca una coincidencia
     Afiliado.findOne(criteria, function(err, afil){
+        //En caso de error
         if(err){
             res.json({
                 status: 0,
                 message: 'error'
             })
         }
-
+        //En caso de que no se encuentre un afiliado
         if(!afil) {
             res.json({
                 status: 0,
@@ -86,6 +101,8 @@ afiliadoCtrl.getAfiliadoByDni = async(req, res) => {
             })
         }
         else{
+        //En caso de que se encuentre una coincidencia
+        //Retorna un status 1 y los datos de afiliado en la variable result    
             res.json({
                 status: 1,
                 message: "success",
