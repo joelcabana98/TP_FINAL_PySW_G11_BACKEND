3927e4d5dc4fc3f88a6d381da4935ebcd6421f97
joelxcabana <joelcabana98@gmail.com>
2020-07-07 00:42:27 -0300

    Verificacion de Pago con Token y perfil de usuario

--- routes/pago.routes.js
+++ routes/pago.routes.js
@@ -4,17 +4,18 @@ const router = express.Router();
 
 //defino controlador para el manejo de CRUD
 const pagoCtrl = require('./../controllers/pago.controllers');
+const autCtrl = require('./../controllers/auth.controller');
 
 // definiendo rutas
-router.get('/', pagoCtrl.getPagos);
-router.post('/', pagoCtrl.createPago);
-router.get('/:id', pagoCtrl.getPago);
-router.put('/:id', pagoCtrl.editPago);
-router.delete('/:id', pagoCtrl.deletePago);
+router.get('/', autCtrl.verifyTokenAdmins, pagoCtrl.getPagos);
+router.post('/', autCtrl.verifyTokenAdmins, pagoCtrl.createPago);
+router.get('/:id', autCtrl.verifyTokenAdmins, pagoCtrl.getPago);
+router.put('/:id', autCtrl.verifyTokenAdmins, pagoCtrl.editPago);
+router.delete('/:id', autCtrl.verifyTokenAdmins, pagoCtrl.deletePago);
 
 //rutas de busqueda
-router.post('/buscar/fechas', pagoCtrl.getPagosByDate);
-router.post('/buscar/afiliado', pagoCtrl.getPagosByAfiliado);
+router.post('/buscar/fechas', autCtrl.verifyTokenAdmins, pagoCtrl.getPagosByDate);
+router.post('/buscar/afiliado', autCtrl.verifyTokenAdmins, pagoCtrl.getPagosByAfiliado);
 
 //exportacion del modulo de rutas
 module.exports = router;
